dd2bfca20ec772bd15398a52641488840e38fc95
varsenikyeng <162155406+varsenikyeng@users.noreply.github.com>
2024-04-26 13:44:01 +0400

    Update Fruit.java

--- Snake/src/Fruit.java
+++ Snake/src/Fruit.java
@@ -1,7 +1,7 @@
 public class Fruit extends GameEntity {
 
     public Fruit(Position position){
-        super(EntityColor.RED, position, " \u001B[31m\u25CF\u001B[0m " );
+        super(position, " \uD83C\uDF4E ");
     }
 
 
