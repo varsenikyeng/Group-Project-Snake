2d8ca2f0db978a050c1af5237d3cedaa273d1a69
manekhachatryan5 <163164364+manekhachatryan5@users.noreply.github.com>
2024-05-05 00:22:16 +0400

    Update SnakeConsole.java

--- Snake/src/SnakeConsole.java
+++ Snake/src/SnakeConsole.java
@@ -1,120 +1,30 @@
-import java.util.ArrayList;
-
-public class Snake {
-    private ArrayList<Position> body;
-    private int speed;
-    private String direction;
-    private boolean isAlive;
-
-    public Snake() {
-        this.isAlive = true;
-        this.direction = "d";
-        this.body = new ArrayList<>();
-        this.body.add( new Position(7, 3));
-    }
-    public Position getHead() {
-        return new Position(this.body.getFirst().getRow(), this.body.getFirst().getColumn());
-    }
-
-    public String getDirection() {
-        return this.direction;
-    }
-    public boolean getIsAlive() {
-        return this.isAlive;
-    }
-
-    public ArrayList<Position> getBody() {
-        return this.body;
-    }
-
-    public Snake(Snake that) {
-        this.isAlive = that.isAlive;
-        this.speed = that.speed;
-        this.direction = that.direction;
-        this.body = new ArrayList<>(that.body.size());
-        for (Position pos : that.body) {
-            this.body.add(new Position(pos.getRow(), pos.getColumn()));
-        }
-    }
-
-    public Position getReachablePosition(String direction) {
-        String d = direction.toLowerCase();
-        int row;
-        int column;
-        switch (d) {
-            case "w":
-                row = this.getHead().getRow() - 1;
-                column = this.getHead().getColumn();
-                break;
-            case "s":
-                row = this.getHead().getRow() + 1;
-                column = this.getHead().getColumn();
-                break;
-            case "a":
-                row = this.getHead().getRow();
-                column = this.getHead().getColumn() - 1;
-                break;
-            case "d":
-                row = this.getHead().getRow();
-                column = this.getHead().getColumn() + 1;
-                break;
-            default:
-                row = this.getHead().getRow();
-                column = this.getHead().getColumn();
-                break;
-        }
-        return new Position(row, column);
-    }
-
-    public Move generateFromPositionAndDirection(String direction) {
-        Position newDestination = this.getReachablePosition(direction);
-        return new Move(this.getHead(), newDestination);
-    }
-
-    public void moveHead(Position newPosition) {
-        this.body.set(0, newPosition);
-    }
-
-    public void grow() {
-        Position tail = this.body.getLast();
-        Position newSegment = null;
-        if (this.body.size() > 1) {
-            Position secondLastSegment = this.body.get(body.size() - 2);
-            int rowOffset = tail.getRow() - secondLastSegment.getRow();
-            int columnOffset = tail.getColumn() - secondLastSegment.getColumn();
-            newSegment = new Position(tail.getRow() + rowOffset,
-                    tail.getColumn() + columnOffset);
-        } else {
-            switch (this.direction) {
-                case "w":
-                    newSegment = new Position(tail.getRow() + 1, tail.getColumn());
-                    break;
-                case "s":
-                    newSegment = new Position(tail.getRow() - 1, tail.getColumn());
-                    break;
-                case "a":
-                    newSegment = new Position(tail.getRow(), tail.getColumn() + 1);
-                    break;
-                case "d":
-                    newSegment = new Position(tail.getRow(), tail.getColumn() - 1);
-                    break;
-//                default:
-//                    return;
+import java.util.Scanner;
+public class SnakeConsole {
+    private SnakeGame game;
+    public void play() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            this.game = new SnakeGame();
+            game.printBoard();
+            while (game.getSnake().getIsAlive()) {
+                String input = sc.next();
+                    if (isValidInput(input)) {
+                        Move move = game.getSnake().generateFromPositionAndDirection(input);
+                        if (game.performMove(move)) {
+                            game.getSnake().setDirection(input);
+                            game.printBoard();
+                            System.out.println(game.getSnake().getDirection());
+                        }
+                    }
             }
-        }
-        this.body.add(newSegment);
-        this.body.trimToSize();
-    }
-    public void shrink() throws OutOfBoundMoveException {
-        if (this.body.size() > 1) {
-            this.body.removeLast();
-        } else {
-            this.isAlive = false;
-            throw new OutOfBoundMoveException("Snake got poisoned(");
+            sc.close();
+        } catch (OutOfBoundMoveException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
     }
-    public void setDirection(String direction) {
-        this.direction = direction;
+    public boolean isValidInput (String input){
+        return input.equalsIgnoreCase("w") || input.equalsIgnoreCase("s")
+                || input.equalsIgnoreCase("a") || input.equalsIgnoreCase("d");
     }
-
 }
