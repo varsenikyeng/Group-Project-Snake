68f981341576dce4b87269bb86b520d9b97f50ac
varsenikyeng <162155406+varsenikyeng@users.noreply.github.com>
2024-05-07 13:44:46 +0400

    Update ScoreManager.java
    ScoreManager changes

--- Snake/src/snake/core/ScoreManager.java
+++ Snake/src/snake/core/ScoreManager.java
@@ -4,11 +4,12 @@ import java.io.*;
 
 public class ScoreManager {
     private static final String SCORE_FILE_1 = "Snake/src/snake/core/snakeGameScore.txt";
-    private static final String SCORE_FILE_2 = "hardSnakeGameScore.txt";
+    private static final String SCORE_FILE_2 = "Snake/src/snake/core/hardSnakeGameScore.txt";
 
-    public static int getHighestScore() {
+    public static int getHighestScore(String difficulty) {
+        String file = (difficulty.equals("worm")) ? SCORE_FILE_1 : SCORE_FILE_2;
         try {
-            Scanner scanner = new Scanner(new File(SCORE_FILE_1));
+            Scanner scanner = new Scanner(new File(file));
             if(scanner.hasNextInt()) {
                 return scanner.nextInt();
             }
@@ -18,17 +19,18 @@ public class ScoreManager {
         }
         return 0;
     }
-    public static void updateScore(int currentScore){
-        int highestScore = getHighestScore();
+    public static void updateScore(int currentScore, String difficulty){
+        String file = (difficulty.equals("worm")) ? SCORE_FILE_1 : SCORE_FILE_2;
+        int highestScore = getHighestScore(difficulty);
         if(currentScore > highestScore){
             PrintWriter writer = null;
             try{
-                writer =  new PrintWriter(new FileOutputStream(SCORE_FILE_1, false));
+                writer =  new PrintWriter(new FileOutputStream(file, false));
                 writer.print(currentScore);
                 writer.flush();
                 writer.close();
             } catch(FileNotFoundException e){
-                System.out.println("Error opening the file " + SCORE_FILE_1);
+                System.out.println("Error opening the file " + file);
                 System.exit(0);
             }
         }
